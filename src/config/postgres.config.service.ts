71055e75517674ce542e68568c857a5fbc83e6f5
Diego Gervasio <diegox300@gmail.com>
2024-06-13 12:29:51 -0300

    Conectando Vercel com supabase

--- src/config/postgres.config.service.ts
+++ src/config/postgres.config.service.ts
@@ -11,7 +11,7 @@ export class PostgresConfigService implements TypeOrmOptionsFactory {
             host: this.configService.get<string>('POSTGRES_HOST'),
             port: this.configService.get<number>('DB_PORT'),
             username: this.configService.get<string>('POSTGRES_USER'),
-            password: this.configService.get<string>('POSTGRES_PASSWORD'),
+            password: this.configService.get<string>('POSTGRES_PASSWORD2'),
             database: this.configService.get<string>('POSTGRES_DATABASE'),
             entities: [__dirname + '/../**/*.entity{.js,.ts}'],
             synchronize: true
